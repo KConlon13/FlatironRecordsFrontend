bc4311b88cd233d64ff8e5bc2046c012441ba4f1
KConlon13 <KyleConlon13@gmail.com>
2019-08-20 08:34:59 -0400

    initial commit

--- src/index.js
+++ src/index.js
@@ -20,10 +20,10 @@ document.addEventListener("DOMContentLoaded", (e) => {
 
             label.artists.forEach(artist => {
                 labelArtists.insertAdjacentHTML("beforeend", `
-                <div class="artistCard">
+                <div class="artistCard" data-id=${artist.id}>
                 <h2>${artist.name}</h2>
-                <p>Genre: ${artist.genre}<p>
-                <p>Bio: ${artist.biography}<p>
+                <p>Genre: ${artist.genre}</p>
+                <p>Bio: ${artist.biography}</p>
                 <img src=${artist.imageUrl} class="artist-image"/>
                 <br>
                 <button data-id=${artist.id} class="delete-btn">Drop Artist</button>
@@ -34,45 +34,58 @@ document.addEventListener("DOMContentLoaded", (e) => {
     
     function renderLabel(label){
         labelContainer.insertAdjacentHTML("beforeend", `
-        <div class="labelCard">
+        <div class="labelCard" data-id=${label.id}>
         <h2>${label.name}</h2>
-        <p>${label.description}<p>
+        <p>${label.description}</p>
         <button data-id=${label.id} class="delete-btn">Dissolve Label</button>
         <h3>Artists:</h3>
         <div id="label-artists" data-id=${label.id}></div>
+        </div>
         `)
         allLabels.push(label)
     }
     
-    // Create Artist
-    form.addEventListener("submit", (e)=> {
-        e.preventDefault()
-        createArtist(e.target)
-    })
+    // Create New Artist
+    // form.addEventListener("submit", (e)=> {
+    //     e.preventDefault()
+    //     console.log("I've been clicked!")
+    //     createArtist(e.target)
+    // })
 
-    function createArtist(artistDetails){
-        return fetch(`${artistUrl}`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                "Accept": "application/json"
-            },
-            body: JSON.stringify({
-                name: artistDetails.artist.value,
-                biography: artistDetails.bio.value,
-                imageUrl: artistDetails.image.value,
-                labelId: artistDetails.label.value        
+    // function createArtist(form){
+    //     debugger
+    //     return fetch(`${artistUrl}`, {
+    //         method: "POST",
+    //         headers: {
+    //             "Content-Type": "application/json",
+    //             "Accept": "application/json"
+    //         },
+    //         body: JSON.stringify({
+    //             name: form.artist.value,
+    //             genre: form.genre.value,
+    //             biography: form.biography.value,
+    //             imageUrl: form.image.value,
+    //             labelId: form.label.value        
+    //         })
+    //     })
+    //     .then(response => response.json())
+    //     .then(data => {
+    //         console.log(data)
+    //         form.reset()
+    //     })
+    // }
+
+    /// Delete Button, deletes but does not persist
+    labelContainer.addEventListener("click", (e) => {
+        let id = e.target.dataset.id
+
+        if (e.target.className === "delete-btn"){
+            e.target.parentNode.remove()
+            fetch(`http://localhost:3000/artists/${id}`, {
+                method: "DELETE"
             })
-        })
-        .then(response => response.json())
-        .then(data => {
-            renderLabel(data)
-            form.reset()
-        })
-    }
-
-
-
+        }
+    })
 
 
 
